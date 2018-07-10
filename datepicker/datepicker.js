c0527d5a8803edd05b54e685e69728f8464c07da
Nick <Nickyshibaev@gmail.com>
2018-07-11 01:01:47 +0300

    css

--- datepicker/datepicker.js
+++ datepicker/datepicker.js
@@ -12,7 +12,7 @@
             '</div>',
         defaults = {
             classes: '',
-            inline: false,
+            inline: true,
             language: 'ru',
             startDate: new Date(),
             firstDay: '',
