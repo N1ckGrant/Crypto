1b404e0eef7339b76e599b6b7031d4e1e740836e
Nick <Nickyshibaev@gmail.com>
2018-07-11 14:44:52 +0300

    css

--- js/my_tabs.js
+++ js/my_tabs.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
   console.log('hereeeee')
   
-  $('div.tabs a').click(function(){
+  $('div.tabs span').click(function(){
 
     var tab_id = $(this).attr('data-tab');
     console.log('click')
 
-    $('div.tabs a').removeClass('current');
+    $('div.tabs span').removeClass('current');
     $('.tab-content').removeClass('current');
 
     $(this).addClass('current');
@@ -15,14 +15,31 @@ $(document).ready(function(){
 
 })
 $(document).ready(function(){
+  console.log('three')
+  
+  $('div.tabs_sell label').click(function(){
+
+    var tab_id = $(this).attr('data-tab');
+    console.log('click_two2')
+
+    $('div.tabs_sell label').removeClass('current2');
+    $('.tab-content_sell_buy').removeClass('current2');
+
+    $(this).addClass('current2');
+    $("#"+tab_id).addClass('current2');
+  })
+
+})
+
+$(document).ready(function(){
   console.log('two')
   
-  $('div.tabs label').click(function(){
+  $('div.subtabs label').click(function(){
 
     var tab_id = $(this).attr('data-tab');
     console.log('click_two')
 
-    $('div.tabs label').removeClass('current');
+    $('div.subtabs label').removeClass('current');
     $('.tab-content_right').removeClass('current');
 
     $(this).addClass('current');
@@ -30,3 +47,5 @@ $(document).ready(function(){
   })
 
 })
+
+
